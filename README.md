395b6cd212ce66e434b76680e961c28c7c6534ac
SolorzanoBryan <basolorzano1@espe.edu.ec>
2022-06-05 17:09:15 -0500

    Primer commit del Agente Piñon

---
+++ README.md
@@ -0,0 +1,1 @@
+# agentePinion - IA
---
+++ agentePinion.py
@@ -0,0 +1,4 @@
+# Creador: Bryan Alfredo Solórzano Montero
+
+# Planteamiento: Se trata de un tipo de agente capaz de determinar el lso cambios correctos de los piñones en las bicicletas para facilitarle a los principiante ese proceso de adaptación
+
