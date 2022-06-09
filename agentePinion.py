63b4705ec5fa2938892b3d0e14b96eb427fa853d
SolorzanoBryan <basolorzano1@espe.edu.ec>
2022-06-08 19:21:47 -0500

    Creación de funciones para logica_absoluta, verticalRandom, cambios y resolver

--- agentePinion.py
+++ agentePinion.py
@@ -4,6 +4,7 @@
 
 # Librerias necesarias
 import time
+import random
 
 '''
 El agente sera capaz de detectar 7 tipo de inclinaciones frontales
@@ -11,10 +12,11 @@ Estamos hablando de temas de escala consideradas de la siguiente forma
 0: 
 
 '''
-DISTANCIA_HORIZONTAL = 40
 distancia_vertical = 0
-pinion = {'one': 0, 'two': 0, 'three': 0, 'four': 0, 'five': 0, 'six': 0, 'seven': 0}
-plato = {'one': None, 'two': None, 'three': None}
+registrar_pinion = []
+registrar_plato = []
+pinion = {'uno': 0, 'dos': 0, 'tres': 0, 'cuatro': 0, 'cinco': 0, 'seis': 0, 'siete': 0}
+plato = {'uno': None, 'dos': None, 'tres': None}
 
 '''
     FORMULA PARA calcular el porcentaje de inclinación 
@@ -24,10 +26,72 @@ plato = {'one': None, 'two': None, 'three': None}
 '''
 index = 0
 
-for pin in pinion:
-    index += 1
-    print(index, pin, pinion[pin])
+# for pin in pinion:
+#     index += 1
+#     print(index, pin, pinion[pin])
 
-for pla in plato:
-    plato[pla] = 1
-    print(pla, plato[pla])
+# for pla in plato:
+#     plato[pla] = 1
+#     print(pla, plato[pla])
+
+def logica_absoluta():
+    distancia_horizontal = float(input("Introduzca cada cuantos metros desea que se haga el escaneo del ambiente para poder hacer los cambios a los piñones: ") )
+    repeticiones = int(input(f"Introduzca la cantidad de repeticiones de {distancia_horizontal} metros") )
+   
+    for num in range(0, repeticiones):
+        vertical = verticalRandom()
+        porcentaje_inclinacion = round((vertical/distancia_horizontal) * 100)
+        
+        resultado_comparatoria = resolver(porcentaje_inclinacion)
+        pin = resultado_comparatoria.get('pinion')
+        pla = resultado_comparatoria.get('plato')
+        dificultad = resultado_comparatoria.get('deficultad')
+        cambios(pin, pla)
+        if not dificultad:
+            print(f'En estos {distancia_horizontal} metros su grado de dificulad sera del {porcentaje_inclinacion} %')
+            print(f'La combinación sera el piñon {pin} y el plato {pla}')
+        else:
+            print(f'En estos {distancia_horizontal} metros su grado de dificulad sera del {porcentaje_inclinacion} %')
+            print(f'La combinación sera el piñon {pin} y el plato {pla}')
+
+        registrar_pinion.append(pinion)
+        registrar_plato.append(plato)
+
+def verticalRandom():
+    num = random.uniform(0,4)
+    return round(num, 2)
+
+def cambios(pin_entrada, plato_entrada):
+    for pin in pinion:
+        if pin == pin_entrada:
+            pinion[pin] = 1
+        else:
+            pinion[pin] = 0
+    for pla in plato:
+        if pla == plato_entrada:
+            pinion[pla] = 1
+        else:
+            pinion[pla] = 0
+
+def resolver(porcentaje_inclinacion):
+    if porcentaje_inclinacion == 0:
+        return {"pinion": "uno", "plato": "tres", "dificultad": False}
+    else:
+        if porcentaje_inclinacion <= 3:
+            return {"pinion": "dos", "plato": "tres", "dificultad": False}
+        else:
+            if porcentaje_inclinacion <= 6:
+                return {"pinion": "tres", "plato": "dos", "dificultad": False}
+            else:
+                if porcentaje_inclinacion <= 9:
+                    return {"pinion": "cuatro", "plato": "dos", "dificultad": True}
+                else:
+                    if porcentaje_inclinacion <= 15:
+                        if porcentaje_inclinacion < 13:
+                            return {"pinion": "cinco", "plato": "dos", "dificultad": True}
+                        else:
+                            return {"pinion": "seis", "plato": "uno", "dificultad": True}
+                    else:
+                        return {"pinion": "siete", "plato": "uno", "dificultad": True}
+
+logica_absoluta()
